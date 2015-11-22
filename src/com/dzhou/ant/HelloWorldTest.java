311d85a26f0facb0fa6f234a7ffc0647a98369d2
Zhou, Hui <huizhou@amazon.com>
2015-11-22 15:23:45 -0500

    First check in.

---
+++ src/com/dzhou/ant/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package com.dzhou.ant;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testNothing() {
+		System.out.println("HelloWorldTest.testNothing");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/dzhou/ant/Test.java
@@ -0,0 +1,8 @@
+package com.dzhou.ant;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
